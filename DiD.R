ed3a6af136d817362d4f8645f967552e183c582b
yuequanguo <145439326+yuequanguo@users.noreply.github.com>
2024-06-27 19:19:15 +0200

    updated DiD via GT (year-month; w/o covariates)

--- DiD.R
+++ DiD.R
@@ -18,7 +18,7 @@
 	rm(list=ls())
 
 
-# sink("DID_FE_240623.txt")
+# sink("DiD_GT_240627.txt")
 # read in clean data
 	dt <- read_dta("June26audit variables for analysis.dta")
 
@@ -47,17 +47,26 @@
 	dt3 <- dt %>%
 		mutate(
 			fid = as.integer(FactoryAssessedID),
-			AssesmentDate = dmy(AssesmentDate),
-			y = format(AssesmentDate, "%Y") %>% as.integer,
+			AssesmentDate = dmy(AssesmentDate),	y = year(AssesmentDate), m = month(AssesmentDate),
 			ym = format(AssesmentDate, "%Y%m") %>% as.integer,
+			ym2 = str_c(y, ceiling(m/2)) %>% as.integer,
+			yq = str_c(y, quarter(AssesmentDate)) %>% as.integer,
 			T1 = if_else(
 				Country%in%c("Vietnam","Jordan","Indonesia","Haiti","Nicaragua"),
 				2017,0
-				), # uniform treatment
+				),
 			T1_ym = if_else(
 				Country%in%c("Vietnam","Jordan","Indonesia","Haiti","Nicaragua"),
 				201707,0
-				), # uniform treatment
+				),
+			T1_ym2 = if_else(
+				Country%in%c("Vietnam","Jordan","Indonesia","Haiti","Nicaragua"),
+				20174,0
+				),
+			T1_yq = if_else(
+				Country%in%c("Vietnam","Jordan","Indonesia","Haiti","Nicaragua"),
+				20173,0
+				),
 			T2 = case_when(
 				Country=="Vietnam" 			~ 2016, # AssesmentDate>=ymd(20160601)
 				Country=="Jordan" 			~ 2016, # AssesmentDate>=ymd(20161101)
@@ -65,7 +74,7 @@
 				Country=="Haiti" 				~ 2017, # AssesmentDate>=ymd(20170701)
 				Country=="Nicaragua" 		~ 2018, # AssesmentDate>=ymd(20180101)
 				TRUE 										~ 0
-				), # staggered treatment
+				),
 			T2_ym = case_when(
 				Country=="Vietnam" 			~ 201606, # AssesmentDate>=ymd(20160601)
 				Country=="Jordan" 			~ 201611, # AssesmentDate>=ymd(20161101)
@@ -73,49 +82,122 @@
 				Country=="Haiti" 				~ 201707, # AssesmentDate>=ymd(20170701)
 				Country=="Nicaragua" 		~ 201801, # AssesmentDate>=ymd(20180101)
 				TRUE 										~ 0
-				) # staggered treatment
+				),
+			T2_ym2 = case_when(
+				Country=="Vietnam" 			~ 20163, # AssesmentDate>=ymd(20160601)
+				Country=="Jordan" 			~ 20166, # AssesmentDate>=ymd(20161101)
+				Country=="Indonesia" 		~ 20171, # AssesmentDate>=ymd(20170101)
+				Country=="Haiti" 				~ 20174, # AssesmentDate>=ymd(20170701)
+				Country=="Nicaragua" 		~ 20181, # AssesmentDate>=ymd(20180101)
+				TRUE 										~ 0
+				),
+			T2_yq = case_when(
+				Country=="Vietnam" 			~ 20162, # AssesmentDate>=ymd(20160601)
+				Country=="Jordan" 			~ 20164, # AssesmentDate>=ymd(20161101)
+				Country=="Indonesia" 		~ 20171, # AssesmentDate>=ymd(20170101)
+				Country=="Haiti" 				~ 20173, # AssesmentDate>=ymd(20170701)
+				Country=="Nicaragua" 		~ 20181, # AssesmentDate>=ymd(20180101)
+				TRUE 										~ 0
+				)
 			)
-	# with(dt3, table(y, T1, exclude=NULL))
 	# str(dt3$ym)
+	# with(dt3, table(ym2, exclude=NULL))
+	# subset(dt3, subset=!is.na(Country)) %>% summarize(n=n(), .by=c(Country, ym)) %>% pivot_wider(names_from=Country, values_from=n) %>% print(n=Inf)
 
 
 
 # DiD by Callaway and Sant'Anna (2021)
+## group-time ATEs for each of three outcomes
+	dvs <- c("reportedcompl","similarCPcompl","distantCPcompl")
+
+	m_gt <- lapply(dvs, function(dv) {
+		att_gt(
+			yname = dv,
+			idname = "fid",
+			tname = "ym",
+			gname = "T2_ym",
+			xformla = NULL,
+			data = dt3,
+			panel = TRUE,
+			allow_unbalanced_panel = TRUE,
+			control_group = "nevertreated", #"notyettreated"
+			clustervars = NULL,
+			est_method = "dr"
+			)
+		})
+
+
 ## reported
+	m_r_t <- aggte(m_gt[[1]], type="dynamic", na.rm=TRUE)
+	summary(m_r_t)
+	ggdid(m_r_t) + scale_x_continuous(expand=c(0.01,0), n.breaks=20) + labs(title="Reported", x="Exposure Periods")
+	# ggsave("DiD_reported_dynamic.png")
+
+	m_r_g <- aggte(m_gt[[1]], type="group", na.rm=TRUE)
+	summary(m_r_g)
+	ggdid(m_r_g) + scale_y_discrete(labels=c("Vietnam","Jordan","Indonesia","Haiti","Nicaragua")) + labs(title="Reported", y="Countries")
+	# ggsave("DiD_reported_group.png")
+
+
+## similar
+	m_s_t <- aggte(m_gt[[2]], type="dynamic", na.rm=TRUE)
+	summary(m_s_t)
+	ggdid(m_s_t) + scale_x_continuous(expand=c(0.01,0), n.breaks=20) + labs(title="Similar", x="Exposure Periods")
+	# ggsave("DiD_similar_dynamic.png")
+
+	m_s_g <- aggte(m_gt[[2]], type="group", na.rm=TRUE)
+	summary(m_s_g)
+	ggdid(m_s_g) + scale_y_discrete(labels=c("Vietnam","Jordan","Indonesia","Haiti","Nicaragua")) + labs(title="Similar", y="Countries")
+	# ggsave("DiD_similar_group.png")
+
+
+## distant
+	m_d_t <- aggte(m_gt[[3]], type="dynamic", na.rm=TRUE)
+	summary(m_d_t)
+	ggdid(m_d_t) + scale_x_continuous(expand=c(0.01,0), n.breaks=20) + labs(title="Distant", x="Exposure Periods")
+	# ggsave("DiD_distant_dynamic.png")
+
+	m_d_g <- aggte(m_gt[[3]], type="group", na.rm=TRUE)
+	summary(m_d_g)
+	ggdid(m_d_g) + scale_y_discrete(labels=c("Vietnam","Jordan","Indonesia","Haiti","Nicaragua")) + labs(title="Distant", y="Countries")
+	# ggsave("DiD_distant_group.png")
+# sink()
+
+## TESTING CODES
 	m_r <- att_gt(
 		yname = "reportedcompl",
 		idname = "fid",
-		tname = "ym",
-		gname = "T2_ym",
-		xformla = NULL,
+		tname = "ym2",
+		gname = "T2_ym2",
+		xformla = ~union+mng,
 		data = dt3,
 		panel = TRUE,
 		allow_unbalanced_panel = TRUE,
 		control_group = "nevertreated", #"notyettreated"
 		clustervars = NULL,
-		est_method = "dr"
+		est_method = "ipw"
 		)
 
 	summary(m_r)
 	ggdid(m_r) + theme(axis.text.y=element_text(size=10), axis.text.x=element_text(angle=90, vjust=0.5, hjust=1, size=10))
 
-	agg.simple <- aggte(m_r, type="simple", na.rm=TRUE)
-	summary(agg.simple)
+	agg.es <- aggte(m_r, type="dynamic", na.rm=TRUE)
+	summary(agg.es)
+	ggdid(agg.es) + scale_x_continuous(expand=c(0.01,0), n.breaks=10)
+	# ggsave("DID_GT_dynamic.png")
+
+	# agg.cs <- aggte(m_r, type="calendar", na.rm=TRUE)
+	# summary(agg.cs)
+	# ggdid(agg.cs) + scale_x_continuous(expand=c(0.01,0), guide=guide_axis(angle=90), n.breaks=10)
+	# ggsave("DID_GT_calendar.png")
 
 	agg.gs <- aggte(m_r, type="group", na.rm=TRUE)
 	summary(agg.gs)
 	ggdid(agg.gs) + scale_y_discrete(labels=c("Vietnam","Jordan","Indonesia","Haiti","Nicaragua"))
-	ggsave("DID_GT_group.png")
+	# ggsave("DID_GT_group.png")
 
-	agg.es <- aggte(m_r, type="dynamic", na.rm=TRUE)
-	summary(agg.es)
-	ggdid(agg.es) + scale_x_continuous(expand=c(0.01,0), n.breaks=10)
-	ggsave("DID_GT_dynamic.png")
-
-	agg.cs <- aggte(m_r, type="calendar", na.rm=TRUE)
-	summary(agg.cs)
-	ggdid(agg.cs) + scale_x_continuous(expand=c(0.01,0), guide=guide_axis(angle=90), n.breaks=10)
-	ggsave("DID_GT_calendar.png")
+	# agg.simple <- aggte(m_r, type="simple", na.rm=TRUE)
+	# summary(agg.simple)
 
 
 
@@ -163,7 +245,7 @@
 	summary(m_d_2m, vcov=function(x) vcovHC(x, method="ar"))
 	m_d_2b <- plm(distantCPcompl ~ T2*b1i + union + CBA + femalepc + regularwkpc + size + factoryageln + Cycle, data=dt2, effect="twoways", model="within", index=c("fid","ym")) #  + mng
 	summary(m_d_2b, vcov=function(x) vcovHC(x, method="ar"))
-# sink()
+
 
 ## coef plots
 	m <- list(
